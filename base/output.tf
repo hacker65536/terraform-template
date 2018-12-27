115ff590aa07647ddcf690385e42f78eaa811db8
shacker <s.hacker65536@gmail.com>
2018-12-27 03:13:49 +0000

    fix

--- base/output.tf
+++ base/output.tf
@@ -9,3 +9,11 @@ output "key_pair" {
 output "data_s3" {
   value = "${aws_s3_bucket.s3.id}"
 }
+
+output "subnet_nat" {
+  value = "${aws_subnet.pri.*.id}"
+}
+
+output "sec_sg" {
+  value = "${aws_security_group.sec.id}"
+}
