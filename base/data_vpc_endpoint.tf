a72f5a4a7a69a3db1a904df1fe4e0d669aadf4a9
shacker <s.hacker65536@gmail.com>
2019-02-20 02:43:43 +0000

    fix

--- base/data_vpc_endpoint.tf
+++ base/data_vpc_endpoint.tf
@@ -9,3 +9,19 @@ data "aws_vpc_endpoint_service" "secretsmanager" {
 data "aws_vpc_endpoint_service" "codebuild" {
   service = "codebuild"
 }
+
+data "aws_vpc_endpoint_service" "codepipeline" {
+  service = "codepipeline"
+}
+
+data "aws_vpc_endpoint_service" "ecr_api" {
+  service = "ecr.api"
+}
+
+data "aws_vpc_endpoint_service" "ecr_dkr" {
+  service = "ecr.dkr"
+}
+
+data "aws_vpc_endpoint_service" "logs" {
+  service = "logs"
+}
